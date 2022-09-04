5ea3e5799147cba82aa7430be7c78863d4165990
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-09-04 00:55:51 -0300

    Created Navbar, Footer, and Button components. Set global font and color variables.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
